d2fadbe74f67f23a84c37f7364e8eacb6d61cea0
gGerz <mr_gzag@mail.ru>
2020-10-31 18:38:17 +0700

    first commit

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueQrcodeReader from 'vue-qrcode-reader'
 
 Vue.config.productionTip = false
+Vue.use(VueQrcodeReader)
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Welcome to the QR</h1>
+    <button @click="openCamera">{{ isShowingCamera ? 'Exit' : 'Scan QE Code'}}</button>
+    <qrcode-stream v-if="isShowingCamera" @decode="onDecode"></qrcode-stream>
+
+    <h5>Полученный контент: <span v-html="qrContent"></span></h5>
+
+
+    <h3>Some random QR</h3>
+    <div class="qr-code-wrapper">
+      <qrcode-vue
+          :value="'idi nahuuuj'"
+          :size="100"
+      >
+      </qrcode-vue>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import QrcodeVue from 'qrcode.vue'
 
 @Component({
   components: {
-    HelloWorld,
+    QrcodeVue
   },
+  data () {
+    return {
+      qrContent: ''
+    }
+  }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+
+  public isShowingCamera = false;
+
+  public openCamera(): void {
+    this.isShowingCamera = !this.isShowingCamera
+  }
+
+  public onDecode(decodedString: string) {
+    console.log(this)
+
+    if (decodedString) {
+      this.qrContent = decodedString
+    }
+    console.log(decodedString)
+  }
+}
 </script>
+
+<style lang="scss">
+
+</style>
